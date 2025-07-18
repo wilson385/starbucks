9e09b5ff35bc11d42edbaff83e9d918e23b9fda3
Wilson Cristian <wilson.cristvao@gmail.com>
2025-07-17 22:23:05 -0300

    alterações

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // document = html
 // querySelector = pegar algoque eu quero no html
 let circulo = document.querySelector(".circulo")
-// cor > variavel que vai armazena as informações do paramento d onclick
-function trocaCor(cor) {
+// cores > variavel que vai armazena as informações do paramento d onclick
+function trocaCor(cores) {
     // altera a cor de fundo do circulo
     // circulo.style.background = "red"
-    circulo.style.background = cor
+    circulo.style.background = cores
     
 }
