ed087c5773df91aa63e3355195a736799fb17ba2
Wilson Cristian <wilson.cristvao@gmail.com>
2025-07-17 22:19:14 -0300

    ajustando o copo grande

---
+++ script.js
@@ -0,0 +1,10 @@
+// document = html
+// querySelector = pegar algoque eu quero no html
+let circulo = document.querySelector(".circulo")
+// cor > variavel que vai armazena as informações do paramento d onclick
+function trocaCor(cor) {
+    // altera a cor de fundo do circulo
+    // circulo.style.background = "red"
+    circulo.style.background = cor
+    
+}
