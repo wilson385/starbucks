ad5af84adb9355d13748ab063035256c80485618
Wilson Cristian <wilson.cristvao@gmail.com>
2025-07-19 17:04:10 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+function sininho (cores) {
+    const body = document.querySelector("body")
+    body.style.background = cores
+}
+
+function trocaCor (coresCirculo) {
+    const circulo = document.querySelector(".circulo")
+    circulo.style.background = coresCirculo
+}
